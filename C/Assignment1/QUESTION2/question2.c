b0fab920aba8d9d7e485c0039e0198f946bfbee3
Anjali Goel <anjalig264@gmail.com>
2019-02-10 13:54:24 +0530

    completed the ques2

---
+++ C/Assignment1/QUESTION2/question2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+	char exit;
+	char text[80];
+	int i, charc = 0, digc = 0, tabc = 0, spacec = 0;
+	printf("Enter the text :\n");
+	scanf_s("%s", &text);
+	for (i = 0; i < 80; ++i);
+	{
+		if (text[i] > 48 && text[i] < 57)
+			digc += 1;
+		else if ((text[i] > 65 && text[i] < 90) || (text[i] > 97 && text[i] < 122))
+			charc += 1;
+		else if (text[i] == 32)
+			spacec += 1;
+		else
+			tabc += 1;
+	}
+	printf("\nCHARACTERS = %d\nDIGITS = %d\nSPACES = %d\nTAB SPACES = %d", charc, digc, spacec, tabc);
+	printf("\nPRESS ENTER TO EXIT");
+	scanf_s("%c", &exit);
+	getchar();
+}
