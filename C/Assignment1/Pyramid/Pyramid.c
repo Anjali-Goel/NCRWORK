c93f7a692340bac7787b273f13ed708f8bdd8812
Anjali Goel <anjalig264@gmail.com>
2019-02-10 16:58:28 +0530

    Done with the pyramid problem

---
+++ C/Assignment1/Pyramid/Pyramid.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+	int i, j;
+	char exit;
+	for (i = 1; i <= 5; ++i)
+	{
+		for (j = 5; j > i; --j)
+			printf(" ");
+		for (j = i; j >= 1; --j)
+			printf("%d", j);
+		for (j = 2; j <= i; ++j)
+			printf("%d", j);
+		for (j = 5; j > i; --j)
+			printf(" ");
+		printf("\n");
+	}
+	scanf_s("%c", &exit);
+	getchar();
+}
