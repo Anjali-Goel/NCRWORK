f5e4196dc0b63bb7f2b91cb84e3e48ff92c53291
Anjali Goel <anjalig264@gmail.com>
2019-02-12 14:15:40 +0530

    Compared the sizes of structures and unions

--- C/Assignment5/Question2/question2.c
+++ C/Assignment5/Question2/question2.c
@@ -1,5 +1,21 @@
 #include<stdio.h>
+struct structure
+{
+	int a;
+	char b;
+}e;
+union uni
+{
+	int c;
+	char d;
+}f;
 main()
 {
-
+	char exit;
+	printf("\nSize of Structure and Union, both with one int and one char member");
+	printf("\nSize of Structure : %d", sizeof(e));
+	printf("\nSize of Union : %d", sizeof(f));
+	printf("\nPress Enter to Exit");
+	scanf_s("%c", &exit);
+	getchar();
 }
