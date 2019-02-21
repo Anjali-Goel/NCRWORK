2db9c24c32530aa9e659379afb8bcec86ac1574d
Anjali Goel <anjalig264@gmail.com>
2019-02-21 11:02:55 +0530

    in progress

--- C++/Assignment2/Question4/overloading.cpp
+++ C++/Assignment2/Question4/overloading.cpp
@@ -22,11 +22,45 @@ public :
 	{
 		cout << "DESTRUCTOR CALLLED";
 	}
-	complx operator+(complx c)
+	complx operator+(complx c)//complex addition
 	{
 		complx t;
 		t.real = real + c.real;
 		t.img = img + c.img;
+		return t;
+	}
+	complx operator-(complx c)//complex numbers subtraction
+	{
+		complx t;
+		t.real = real - c.real;
+		t.img = img - c.img;
+		return t;
+	}
+	complx operator-()//negatiion
+	{
+		complx t;
+		t.real = -real;
+		t.img = -img;
+		return t;
+	}
+	complx operator++()//pre increment
+	{
+		++real;
+		++img;
+		return(*this);
+	}
+	complx operator++(int x)//post increment
+	{
+		complx t;
+		t.real = real++;
+		t.img = img++;
+		return t;
+	}
+	complx operator=(int x)//assignment operation
+	{
+		complx t;
+		t.real = real;
+		t.img = img;
 		return t;
 	}
 
